32e8d36735b5912605af221e4dc7b41ee5563960
dev-Nahian <nahian.bdcalling@gmail.com>
2025-07-27 22:33:11 +0600

    Add Functional done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./shared/Navbar";
+import Footer from "./shared/Footer";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/task/TaskBoard.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import SearchTask from "./SearchTask";
+import TaskAction from "./TaskAction";
+import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
+
+export default function TaskBoard() {
+  const defultData = {
+    id: crypto.randomUUID(),
+    title: "Integration API",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "python", "api"],
+    Priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defultData]);
+  const [isModelOpen, setIsModalOpen] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    console.log("Adding Task", newTask);
+    setTasks([...tasks, newTask]);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <section className="mb-20" id="tasks">
+      {isModelOpen && <AddTaskModal onSave={handleAddTask} />}
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          {/* <SearchTask /> */}
+        </div>
+
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction handleOnAdd={() => setIsModalOpen(true)} />
+          <TaskList tasks={tasks} />
+        </div>
+      </div>
+    </section>
+  );
+}
